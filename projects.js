81aa8b4fa47bc32c90b5d9b9c275305eef173a81
Noah Reynolds <betajsion@gmail.com>
2023-06-23 14:08:30 -0400

    Added work on the projects page and added the js file for it.

--- projects.js
+++ projects.js
@@ -1,20 +1,18 @@
-var menu_icon = document.querySelector(".menu-icon");
+var nav_icon = document.querySelector(".nav-icon");
 var menu_items = document.getElementsByClassName("menu");
-var nav_bar = document.querySelector(".nav-bar");
+
 
 var active = true;
-menu_icon.onclick = function() {
+nav_icon.onclick = function() {
 	if(active) {
 		active = !active;
 		for(var i = 0; i < menu_items.length; i++) {
 			menu_items[i].style.display = "none";
-			nav_bar.style.width = "5%";
 		}
 	}else {
 		active = !active;
 		for(var i = 0; i < menu_items.length; i++) {
 			menu_items[i].style.display = "block";
-			nav_bar.style.width = "10%";
 		}
 	}
 
